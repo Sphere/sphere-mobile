9f6442f15504f775fa42baa51f7dca1dc514a161
amantarento <amankumar.sharma@tarento.com>
2023-01-10 18:36:36 +0530

    commit the local-host.proxy.json

--- src/app/modules/public/components/public-home/public-home.component.ts
+++ src/app/modules/public/components/public-home/public-home.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { NavigationExtras, Router } from '@angular/router';
-
 @Component({
   selector: 'app-public-home',
   templateUrl: './public-home.component.html',
@@ -13,50 +10,10 @@ export class PublicHomeComponent implements OnInit {
   content: any;
 
   constructor(
-    private http: HttpClient,
-    private router: Router
   ) { }
 
   ngOnInit() {
-    this.getCourses();
+   
   }
-  async getCourses() {
-   let postData = {
-      "request": {
-          "filters": {
-              "primaryCategory": [
-                  "Course"
-              ],
-              "contentType": [
-                  "Course"
-              ],
-              "status": [
-                  "Live"
-              ]
-          },
-          "sort_by": {
-              "lastUpdatedOn": "desc"
-          }
-      },
-      "query": "",
-      "sort": [
-          {
-              "lastUpdatedOn": "desc"
-          }
-      ]
-  }
-    this.data = await this.http.post('https://sphere.aastrika.org/api/content/v1/search', postData).subscribe(data => {
-      this.content = data['result'].content;     
-     }, error => {
-      console.log(error);
-    });
-  }
-
-  courseDetails(data: any) {
-    const navigationExtras: NavigationExtras = {
-      queryParams: data
-    };
-    //this.router.navigate(['/public/course-overview'], navigationExtras)
-  }
-
+ 
 }
