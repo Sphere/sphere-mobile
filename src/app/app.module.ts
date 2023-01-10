11be2a9cd44a7e08da989479f9b0730a2a8f39bd
amantarento <amankumar.sharma@tarento.com>
2023-01-10 11:34:37 +0530

    push the changes for assessts and import error fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,31 @@ import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { KeycloakAngularModule } from 'keycloak-angular'
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatIconModule,
+  MatMenuModule,
+  MatProgressBarModule,
+  MatGridListModule,
+  MatRippleModule,
+  MatSliderModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MAT_PROGRESS_SPINNER_DEFAULT_OPTIONS,
+  MAT_SNACK_BAR_DEFAULT_OPTIONS,
+  MatInputModule,
+  MatFormFieldModule,
+  MatListModule,
+  MatAutocompleteModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatSelectModule,
+} from '@angular/material'
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +38,8 @@ import { WidgetResolverModule } from '@ws-widget/resolver'
 import {  WIDGET_REGISTERED_MODULES, WIDGET_REGISTRATION_CONFIG } from '@ws-widget/collection'
 import {  ImageCropModule, LoggerService, PipeSafeSanitizerModule } from '@ws-widget/utils'
 import { InitService } from './services/init.service';
+import { AuthKeycloakService } from '../../library/ws-widget/utils/src/lib/services/auth-keycloak.service'
+
 const appInitFactory = (initSvc: InitService, logger: LoggerService)=> async()=>{
   try {
     await initSvc.init()
@@ -22,11 +49,34 @@ const appInitFactory = (initSvc: InitService, logger: LoggerService)=> async()=>
 }
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, 
+  imports: [BrowserModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatMenuModule,
+    MatGridListModule,
+    MatDividerModule,
+    MatProgressBarModule,
+    MatExpansionModule,
+    MatRippleModule,
+    MatDialogModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatTooltipModule,
+    MatAutocompleteModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatExpansionModule, 
+    MatSnackBarModule,
     AppRoutingModule,
     ComponentsModule,
     ImageCropModule,
     PipeSafeSanitizerModule,
+    KeycloakAngularModule,
     IonicModule.forRoot(), 
      ...WIDGET_REGISTERED_MODULES,
     WidgetResolverModule.forRoot(WIDGET_REGISTRATION_CONFIG),],
@@ -36,7 +86,8 @@ const appInitFactory = (initSvc: InitService, logger: LoggerService)=> async()=>
       useFactory: appInitFactory,
       deps: [InitService, LoggerService],
       multi: true,
-    }],
+    },
+    AuthKeycloakService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatIconModule,
+  MatMenuModule,
+  MatProgressBarModule,
+  MatGridListModule,
+  MatRippleModule,
+  MatSliderModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MAT_PROGRESS_SPINNER_DEFAULT_OPTIONS,
+  MAT_SNACK_BAR_DEFAULT_OPTIONS,
+  MatInputModule,
+  MatFormFieldModule,
+  MatListModule,
+  MatAutocompleteModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatSelectModule,
+} from '@angular/material'
 import { IonicModule } from '@ionic/angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { WidgetResolverModule } from '@ws-widget/resolver'
+import {  WIDGET_REGISTERED_MODULES, WIDGET_REGISTRATION_CONFIG } from '@ws-widget/collection'
+
 import { AppPublicNavBarComponent } from './app-public-nav-bar/app-public-nav-bar.component';
 @NgModule({
   declarations: [
@@ -13,7 +40,29 @@ import { AppPublicNavBarComponent } from './app-public-nav-bar/app-public-nav-ba
     ReactiveFormsModule,
     IonicModule,
     ReactiveFormsModule,
-    
+    MatSliderModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatMenuModule,
+    MatGridListModule,
+    MatDividerModule,
+    MatProgressBarModule,
+    MatExpansionModule,
+    MatRippleModule,
+    MatDialogModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatTooltipModule,
+    MatAutocompleteModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatExpansionModule,
+    ...WIDGET_REGISTERED_MODULES,
+    WidgetResolverModule.forRoot(WIDGET_REGISTRATION_CONFIG),
   ],
   entryComponents: [
     AppPublicNavBarComponent
