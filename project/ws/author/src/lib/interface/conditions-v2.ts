8dc8e0f29079e26912a031effbf7da3d70536f7a
mansurskTarento <mansur.shaik@tarento.com>
2023-02-06 14:14:49 +0530

    removing added files to solve packges issues

--- project/ws/author/src/lib/interface/conditions-v2.ts
+++
@@ -1,6 +0,0 @@
-import { NSContent } from '@ws/author/src/lib/interface/content'
-
-export interface IConditionsV2 {
-  fit?: { [key in keyof NSContent.IContentMeta]: any[] }[]
-  notFit?: { [key in keyof NSContent.IContentMeta]: any[] }[]
-}
