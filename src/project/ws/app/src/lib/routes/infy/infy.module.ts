a0de0ad51bffd1f867fc5680ca7c49575ddcc066
mansurskTarento <mansur.shaik@tarento.com>
2023-02-16 11:14:57 +0530

    MatIcon & MatCard issues in build

--- src/project/ws/app/src/lib/routes/infy/infy.module.ts
+++ src/project/ws/app/src/lib/routes/infy/infy.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common'
 
 import { InfyRoutingModule } from './infy-routing.module'
 import { CertificationsGuard } from './routes/certification-dashboard/guards/certifications.guard'
+import { MatIconModule } from '@angular/material/icon'
+import { MatCardModule } from '@angular/material/card'
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, InfyRoutingModule],
+  imports: [CommonModule, InfyRoutingModule, MatIconModule, MatCardModule],
   providers: [CertificationsGuard],
 })
 export class InfyModule {}
--- src/project/ws/app/src/lib/routes/infy/routes/counter/counter.module.ts
+++ src/project/ws/app/src/lib/routes/infy/routes/counter/counter.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 import { CounterEntityComponent } from './components/counter-entity/counter-entity.component'
 
 @NgModule({
   declarations: [CounterEntityComponent],
   imports: [
     CommonModule,
+    MatIconModule,
+    MatCardModule
   ],
 })
 export class CounterModule { }
